18ea8f3632845b8dc84f731c5950c9c43fe8c623
artem <artemnurmetow@gmail.com>
2026-03-02 20:23:57 +0100

    -init: initialised tests

---
+++ src/test/java/core/basesyntax/FruitShopTest.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FruitShopTest {
+
+    @Test
+    void demo() {
+        assertTrue(true);
+    }
+}
