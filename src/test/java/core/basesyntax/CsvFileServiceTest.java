d540e2d942ba3ba29a6d9cc6e62b34f2fbe73b53
artem <artemnurmetow@gmail.com>
2026-03-03 17:37:04 +0100

    -feat(CsvFileServiceTest): implemented FileReader tests logic with 100% coverage

--- src/test/java/core/basesyntax/CsvFileServiceTest.java
+++ src/test/java/core/basesyntax/CsvFileServiceTest.java
@@ -4,23 +4,38 @@ import core.basesyntax.service.FileReader;
 import core.basesyntax.service.FileWriter;
 import core.basesyntax.service.impl.CsvReaderImpl;
 import core.basesyntax.service.impl.CsvWriterImpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CsvFileServiceTest {
     private static FileReader fileReader;
     private static FileWriter fileWriter;
+    private static File fileManager;
+    private static final String DEFAULT_OUTPUT_PATH = "src/test/resources/output.csv";
 
     @BeforeAll
     static void beforeAll() {
         fileReader = new CsvReaderImpl();
         fileWriter = new CsvWriterImpl();
+    }
+
+    @AfterEach
+    void afterEach() {
+        boolean wasDeleted = new File(DEFAULT_OUTPUT_PATH).delete();
     }
 
     @Test
@@ -52,4 +67,81 @@ public class CsvFileServiceTest {
         assertTrue(exception.getMessage().contains("File was not found"));
     }
 
+    @Test
+    void write_fromValidString_Ok() {
+        assertDoesNotThrow(() ->
+                fileWriter.writeTo("Hello, world!", DEFAULT_OUTPUT_PATH));
+    }
+
+    @Test
+    void write_inputEquals_Ok() throws IOException {
+        assertDoesNotThrow(() ->
+                fileWriter.writeTo("Hello, world!", DEFAULT_OUTPUT_PATH));
+
+        String content = Files.readString(Path.of(DEFAULT_OUTPUT_PATH));
+
+        assertEquals("Hello, world!", content);
+    }
+
+    @Test
+    void write_toValidName_Ok() {
+        assertDoesNotThrow(() ->
+                fileWriter.writeTo("Hello, world!", DEFAULT_OUTPUT_PATH));
+    }
+
+    @Test
+    void write_fromNullString_NotOk() {
+        RuntimeException exception =
+                assertThrows(RuntimeException.class, () -> fileWriter.writeTo(null, "example_output.csv"));
+        assertTrue(exception.getMessage().contains("String from which should be written from is empty"));
+    }
+
+    @Test
+    void write_fromEmptyString_NotOk() {
+        RuntimeException exception =
+                assertThrows(RuntimeException.class, () -> fileWriter.writeTo("", "example_output.csv"));
+        assertTrue(exception.getMessage().contains("String from which should be written from is empty"));
+    }
+
+    @Test
+    void write_toNullFilePath_NotOk() {
+        RuntimeException exception =
+                assertThrows(RuntimeException.class, () -> fileWriter.writeTo("Hello, world!", null));
+        assertTrue(exception.getMessage().contains("The File Path that should be written to is empty"));
+    }
+
+    @Test
+    void write_toEmptyFilePath_NotOk() {
+        RuntimeException exception =
+                assertThrows(RuntimeException.class, () -> fileWriter.writeTo("Hello, world!", ""));
+        assertTrue(exception.getMessage().contains("The File Path that should be written to is empty"));
+    }
+
+    @Test
+    void write_toNotExistingFolder_NotOk() {
+        RuntimeException exception =
+                assertThrows(RuntimeException.class, () -> fileWriter.writeTo("Hello, world!",
+                        "src/test/example/example_output.csv"));
+        assertTrue(exception.getMessage().contains("Could not create or write to file"));
+    }
+
+    @Test
+    void write_fileNameWithNotAllowedSymbols_NotOk() {
+        List<String> invalidChars = List.of("*", "\"", "?", "<", ">", "|", "/", "\\");
+        for (String invalidChar : invalidChars) {
+            String fileName = "src/test/resources/example" + invalidChar + "output.csv";
+            RuntimeException exception = assertThrows(RuntimeException.class,
+                    () -> fileWriter.writeTo("Hello, world!", fileName));
+            assertTrue(exception.getMessage().contains("Could not create or write to file"));
+        }
+    }
+
+    @Test
+    void write_inputDoesNotEqual_NotOk() throws IOException {
+        assertDoesNotThrow(() ->
+                fileWriter.writeTo("Hello, world!", DEFAULT_OUTPUT_PATH));
+
+        String content = Files.readString(Path.of(DEFAULT_OUTPUT_PATH));
+        assertNotEquals("Hello world!" , content);
+    }
 }
