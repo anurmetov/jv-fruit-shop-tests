7f4a482dfa65f974a2b121306467a9de4f85a5dd
artem <artemnurmetow@gmail.com>
2026-03-10 17:55:15 +0100

    -feat(ShopServiceImplTest): implemented tests and changed ShopServiceImpl to check more edge cases

--- src/main/java/core/basesyntax/service/impl/ShopServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ShopServiceImpl.java
@@ -18,8 +18,13 @@ public class ShopServiceImpl implements ShopService {
         if (transactions == null) {
             throw new RuntimeException("Transaction list is null");
         }
-
+        if (transactions.isEmpty()) {
+            throw new RuntimeException("Transaction list is empty");
+        }
         for (FruitTransaction transaction : transactions) {
+            if (transaction == null) {
+                throw new RuntimeException("Provided transaction in transaction List is null");
+            }
             OperationHandler handler = operationStrategy.getHandler(transaction.getOperation());
             if (handler == null) {
                 throw new RuntimeException("No handler is found for the transaction's operation.");
---
+++ src/test/java/core/basesyntax/ShopServiceImplTest.java
@@ -0,0 +1,66 @@
+package core.basesyntax;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.ShopService;
+import core.basesyntax.service.impl.ShopServiceImpl;
+import core.basesyntax.strategy.BalanceOperation;
+import core.basesyntax.strategy.OperationHandler;
+import core.basesyntax.strategy.OperationStrategy;
+import core.basesyntax.strategy.OperationStrategyImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShopServiceImplTest {
+    private static ShopService shopService;
+    private static Map<FruitTransaction.Operation, OperationHandler>
+            operationHandler = new HashMap<>();
+    private static OperationStrategy operationStrategy;
+;
+
+    @BeforeAll
+    static void setUp() {
+        operationHandler.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
+        operationStrategy = new OperationStrategyImpl(operationHandler);
+        shopService = new ShopServiceImpl(operationStrategy);
+    }
+
+    @Test
+    void process_validTransactionList_IsOk() {
+        assertDoesNotThrow(() -> shopService.process(List.of(new FruitTransaction(FruitTransaction.Operation.BALANCE,
+                "banana", 5))));
+    }
+
+    @Test
+    void process_transactionListIsNull_NotOk() {
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> shopService.process(null));
+        assertTrue(exception.getMessage().contains("Transaction list is null"));
+    }
+
+    @Test
+    void process_transactionListIsEmpty_NotOk() {
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> shopService.process(List.of()));
+        assertTrue(exception.getMessage().contains("Transaction list is empty"));
+    }
+
+    @Test
+    void process_transactionIsNull_NotOk() {
+        List<FruitTransaction> test = new ArrayList<>();
+        test.add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana" , 5));
+        test.add(null);
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> shopService.process(test));
+        assertTrue(exception.getMessage().contains("Provided transaction in transaction List is null"));
+    }
+
+}
